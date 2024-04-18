5ed5496074183acb5ab31e7c75b990bf9a6d2af4
[SW7] 김지훈 <lanslot84@gmail.com>
2024-04-18 17:10:10 +0900

    Refactor: button 컴포넌트 스타일 적용 리팩토링

--- src/components/atoms/Button.tsx
+++ src/components/atoms/Button.tsx
@@ -20,19 +20,21 @@ export default function Button({
 }) {
   return (
     <>
-      {style === TYPES.NORMAL_STYLE ? (
+      {type === TYPES.LINK ? (
         <Link
           href={link || "#"}
-          className="bg-slate-100 hover:bg-slate-200 active:bg-slate-300 focus:outline-none text-sky-600 font-semibold h-12 px-6 rounded-lg flex items-center justify-center w-auto"
+          className={` ${
+            style === TYPES.NORMAL_STYLE ? `btn-normal` : `btn-primary`
+          } w-auto`}
         >
           {label}
         </Link>
       ) : (
         <button
           type={type === TYPES.SUBMIT ? "submit" : undefined}
-          className={`bg-sky-500 hover:bg-sky-600 active:bg-sky-700 focus:outline-none text-white font-semibold h-12 px-6 rounded-lg flex items-center justify-center ${
-            size ? `w-[${size}px]` : `w-auto`
-          }`}
+          className={` ${
+            style === TYPES.NORMAL_STYLE ? `btn-normal` : `btn-primary`
+          } ${size ? `w-[${size}px]` : `w-auto`}`}
         >
           {label}
         </button>
